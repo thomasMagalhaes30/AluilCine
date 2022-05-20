6412b9f6e250764eebe0f76fe1d1f6e8a06d50e2
kedasilva5 <Kevin.DA_SILVA@etu.uca.fr>
2022-05-20 15:03:46 +0200

    Création de l'entity User

---
+++ aluilcine/src/main/java/fr/iut/aluilcine/entities/User.java
@@ -0,0 +1,40 @@
+package fr.iut.aluilcine.entities;
+
+import org.springframework.data.annotation.Id;
+
+/**
+ * Represente un utilisateur
+ */
+public class User {
+
+    @Id
+    public String id;
+
+    public int schemaVersion;
+
+    public String pseudo;
+
+    public String email;
+
+    public String password;
+
+    public User(){}
+
+    public User(String id, int schemaVersion, String pseudo, String email, String password) {
+        this.id = id;
+        this.schemaVersion = schemaVersion;
+        this.pseudo = pseudo;
+        this.email = email;
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id='" + id + '\'' +
+                ", schemaVersion=" + schemaVersion +
+                ", pseudo='" + pseudo + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
