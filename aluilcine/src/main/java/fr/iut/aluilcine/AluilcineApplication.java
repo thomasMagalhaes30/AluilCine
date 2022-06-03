da1b0422a6384092a475bade761a1cb63547104b
GODZI <thomas.magalhaes00@gmail.com>
2022-06-03 11:53:26 +0200

    suppression des annotations inutiles de la classe Application

--- aluilcine/src/main/java/fr/iut/aluilcine/AluilcineApplication.java
+++ aluilcine/src/main/java/fr/iut/aluilcine/AluilcineApplication.java
@@ -5,17 +5,13 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.mongodb.MongoDatabaseFactory;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.convert.DefaultDbRefResolver;
 import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
 import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
-@ComponentScan("fr.iut.aluilcine") //to scan packages mentioned
-@EnableMongoRepositories("fr.iut.aluilcine.repositories") //to activate MongoDB repositories
 public class AluilcineApplication {
 
 	static Logger logger = LoggerFactory.getLogger(AluilcineApplication.class);
