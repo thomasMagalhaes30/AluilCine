79c7a6f7b3743ccd6f27f5f0339e3c70efedf996
GODZI <thomas.magalhaes00@gmail.com>
2022-06-06 21:16:37 +0200

    Ajout des annotations pour la vérification

--- aluilcine/src/main/java/fr/iut/aluilcine/entities/Review.java
+++ aluilcine/src/main/java/fr/iut/aluilcine/entities/Review.java
@@ -2,16 +2,26 @@ package fr.iut.aluilcine.entities;
 
 import org.springframework.data.annotation.Id;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 /**
  * Represente un commentaire
  */
 public class Review extends BaseEntity{
     private int schemaVersion;
 
+    @NotBlank
+    @Size(min=2, max = 128)
     private String pseudo;
 
+    @NotBlank
     private String message;
-
+    
+    @Min(0)
+    @Max(20)
     private int mark;
 
     private String movieId;
