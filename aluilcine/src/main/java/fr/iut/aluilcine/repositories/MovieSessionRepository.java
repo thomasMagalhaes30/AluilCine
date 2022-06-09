078ebe729d205cb504fd148c8c20dea4d78e9747
Amiralgaby <gaby.theuws@gmail.com>
2022-06-09 11:52:05 +0200

    optimisation de l'endpoint dans MovieSessionsController

--- aluilcine/src/main/java/fr/iut/aluilcine/repositories/MovieSessionRepository.java
+++ aluilcine/src/main/java/fr/iut/aluilcine/repositories/MovieSessionRepository.java
@@ -2,6 +2,11 @@ package fr.iut.aluilcine.repositories;
 
 import fr.iut.aluilcine.entities.MovieSession;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+import java.util.List;
 
 public interface MovieSessionRepository extends MongoRepository<MovieSession, String> {
+    @Query("{ 'cinema_id' : ?0 }")
+    List<MovieSession> findByCinemaId(String cinemaId);
 }
