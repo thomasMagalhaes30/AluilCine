c608c7c37eaf7cb59ccca08fa4def23d171ca9ea
Amiralgaby <gaby.theuws@gmail.com>
2022-06-09 12:34:28 +0200

    [MovieSessionsController] simplification du code en utilisant des queries

--- aluilcine/src/main/java/fr/iut/aluilcine/repositories/MovieSessionRepository.java
+++ aluilcine/src/main/java/fr/iut/aluilcine/repositories/MovieSessionRepository.java
@@ -9,4 +9,10 @@ import java.util.List;
 public interface MovieSessionRepository extends MongoRepository<MovieSession, String> {
     @Query("{ 'cinema_id' : ?0 }")
     List<MovieSession> findByCinemaId(String cinemaId);
+
+    @Query("{ 'movie_id' : ?0 }")
+    List<MovieSession> findByMovieId(String movieId);
+
+    @Query("{'cinema_id': ?0, 'movie_id': ?1}")
+    List<MovieSession> findByCinemaIdAndMovieId(String cinemaId, String movieId);
 }
