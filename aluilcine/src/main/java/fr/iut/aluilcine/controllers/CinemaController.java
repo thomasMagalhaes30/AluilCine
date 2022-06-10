e20a19081bdc99481984d42b37e25174eaeb71ef
Amiralgaby <gaby.theuws@gmail.com>
2022-06-10 12:07:08 +0200

    get by nearest location + javadoc

--- aluilcine/src/main/java/fr/iut/aluilcine/controllers/CinemaController.java
+++ aluilcine/src/main/java/fr/iut/aluilcine/controllers/CinemaController.java
@@ -2,8 +2,16 @@ package fr.iut.aluilcine.controllers;
 
 import fr.iut.aluilcine.entities.Cinema;
 import fr.iut.aluilcine.repositories.CinemaRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+import static org.springframework.http.HttpStatus.OK;
 
 /**
  * CinemaController qui permet la gestion des {@link Cinema cinemas}.
@@ -11,4 +19,57 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/cinemas")
 public class CinemaController extends BaseController<Cinema, CinemaRepository> {
+
+    private static class CinemaDistance {
+        public Cinema cinema;
+        public double rayon;
+
+        public CinemaDistance(Cinema c, double rayon) {
+            this.cinema = c;
+            this.rayon = rayon;
+        }
+    }
+
+    // latitude et longitude sont obligatoire
+    // on doit prendre les 5 les plus proches de la latitude et longitude données
+    /**
+     * Récupère une liste de cinema selon une latitude et une longitude
+     * @param latitude latitude donnée
+     * @param longitude longitude donnée
+     * @return Une liste de cinema trié par odre croissant de distance du point donné
+     */
+    @GetMapping("/location")
+    public ResponseEntity<?> getAllByLocation(@RequestParam("latitude") float latitude,
+                                              @RequestParam("longitude") float longitude,
+                                              @RequestParam(value = "limit", required = false) Integer limit) {
+        if (limit == null)
+            limit = 5; // par défaut la limite est 5
+
+        System.out.println("latitude : " + latitude  + ", longitude : " + longitude);
+
+        List<Cinema> cinemas = repository.findAll();
+        List<CinemaDistance> cinemaDistances = new ArrayList<>();
+
+        for (Cinema c:
+             cinemas) {
+            float cLatitude = c.getLatitude();
+            float cLongitude = c.getLongitude();
+
+            double rayon = Math.sqrt(Math.pow(cLatitude-latitude,2) - Math.pow(cLongitude-longitude,2));
+
+            System.out.println("Rayon calculé pour " + c.getName() + " est de : " + rayon);
+            cinemaDistances.add(new CinemaDistance(c, rayon));
+        }
+
+        cinemaDistances.sort(Comparator.comparingDouble(o -> o.rayon));
+        cinemas = cinemaDistances.stream()
+                .map(p -> p.cinema)
+                .limit((limit <= 0) ? 5 : limit)
+                .collect(Collectors.toList());
+
+        return new ResponseEntity<>(cinemas,OK);
+        // return new ResponseEntity<>(repository.getAllByLocationInternal((float) (latitude-3.14), (float) (latitude+3.14), (float) (longitude-3.14), (float) (longitude +3.14)),OK);
+    }
 }
+
+
--- aluilcine/src/main/java/fr/iut/aluilcine/repositories/CinemaRepository.java
+++ aluilcine/src/main/java/fr/iut/aluilcine/repositories/CinemaRepository.java
@@ -2,9 +2,14 @@ package fr.iut.aluilcine.repositories;
 
 import fr.iut.aluilcine.entities.Cinema;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+import java.util.List;
 
 /**
  * Represente un repository des cinemas
  */
 public interface CinemaRepository extends MongoRepository<Cinema, String> {
+    @Query("{ latitude : { $gte : ?0 , $lte : ?1 } , longitude : { $gte : ?2 , $lte : ?3}}")
+    List<Cinema> getAllByLocationInternal(float latitudeMin, float latitudeMax, float longitudeMin , float longitudeMax);
 }
