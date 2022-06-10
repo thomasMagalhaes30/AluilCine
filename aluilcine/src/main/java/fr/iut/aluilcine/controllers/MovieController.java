69df400b2e20dac0e620cb21e878dba136c17779
kedasilva5 <kevin.DA_SILVA@etu.uca.fr>
2022-06-10 22:15:56 +0200

    Ajout de la méthode top pour les films

--- aluilcine/src/main/java/fr/iut/aluilcine/controllers/MovieController.java
+++ aluilcine/src/main/java/fr/iut/aluilcine/controllers/MovieController.java
@@ -1,14 +1,19 @@
 package fr.iut.aluilcine.controllers;
 
-import fr.iut.aluilcine.entities.Cinema;
+
 import fr.iut.aluilcine.entities.Movie;
-import fr.iut.aluilcine.entities.User;
 import fr.iut.aluilcine.repositories.MovieRepository;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
+
+import static org.springframework.http.HttpStatus.*;
 
 /**
  * CinemaController qui permet la gestion des {@link Movie movies}.
@@ -16,4 +21,19 @@ import java.util.List;
 @RestController
 @RequestMapping("/movies")
 public class MovieController extends BaseController<Movie, MovieRepository> {
+
+    @GetMapping("/top{number}")
+    public ResponseEntity<?> top(@PathVariable("number") int number){
+        try {
+            if(number <= 0 || number > 20)
+                return new ResponseEntity<>("Le nombre doit être entre 1 et 20",NOT_ACCEPTABLE);
+
+            PageRequest request = PageRequest.of(0,number);
+            return new ResponseEntity<>(repository.findByOrderByMarkDesc(request).getContent(), OK);
+
+        }catch (Exception e){
+            customLogError(e.getMessage());
+        }
+        return new ResponseEntity<>(INTERNAL_SERVER_ERROR);
+    }
 }
--- aluilcine/src/main/java/fr/iut/aluilcine/repositories/MovieRepository.java
+++ aluilcine/src/main/java/fr/iut/aluilcine/repositories/MovieRepository.java
@@ -1,12 +1,21 @@
 package fr.iut.aluilcine.repositories;
 
 import fr.iut.aluilcine.entities.Movie;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+import java.util.List;
 
 /**
  * Represente un repository des films
  */
-public interface MovieRepository  extends MongoRepository<Movie, String> {
+public interface MovieRepository extends MongoRepository<Movie, String> {
+    
+    @Query()
+    Page<Movie> findByOrderByMarkDesc(Pageable pageable);
+
 
 
 }
