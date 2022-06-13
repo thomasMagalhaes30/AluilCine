89da83424b6888c8a9ff017c82410d33899ff3c5
andasilva7 <antoine.DA_SILVA@etu.uca.fr>
2022-06-13 14:14:35 +0200

    findReviewsBymovieId + pagination

--- aluilcine/src/main/java/fr/iut/aluilcine/controllers/ReviewController.java
+++ aluilcine/src/main/java/fr/iut/aluilcine/controllers/ReviewController.java
@@ -7,8 +7,11 @@ import fr.iut.aluilcine.repositories.MovieRepositoryCustom;
 import fr.iut.aluilcine.repositories.ReviewRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import static org.springframework.http.HttpStatus.*;
 
 /**
  * Controller permettant la gestion des {@link Review reviews}.
@@ -24,5 +27,35 @@ public class ReviewController extends BaseController<Review, ReviewRepository>{
     public boolean afterAdd(Review entityAdd) {
         movieRepositoryCustom.updateMovieByIdAfterAddReview(entityAdd.getMovieId(), entityAdd.getMark());
         return true;
+    }
+
+
+
+    @GetMapping("/pageableByMovieId")
+    public ResponseEntity<?> getReviewsByMovieIdByPage(@RequestParam(value = "movieId") String movieId,
+                                           @RequestParam(value = "page", required = false) Integer page,
+                                                       @RequestParam(value = "numberOfReviewsByPage", required = false) Integer numberOfReviewsByPage){
+
+    try{
+        if (numberOfReviewsByPage == null) numberOfReviewsByPage = 5;
+
+        if(numberOfReviewsByPage <= 0 || numberOfReviewsByPage > 20)
+            return new ResponseEntity<>("Le nombre doit être entre 1 et 20",NOT_ACCEPTABLE);
+
+        if(page == null){
+            PageRequest request = PageRequest.of(0,numberOfReviewsByPage);
+            return new ResponseEntity<>(repository.findByMovieId(movieId,request), OK);
+        }
+
+        if(page < 0)
+            return new ResponseEntity<>("Le numero de la page doit être positif",NOT_ACCEPTABLE);
+
+        PageRequest request = PageRequest.of(page,numberOfReviewsByPage);
+        return new ResponseEntity<>(repository.findByMovieId(movieId,request), OK);
+
+    }catch (Exception e){
+        customLogError(e.getMessage());
+    }
+        return new ResponseEntity<>(INTERNAL_SERVER_ERROR);
     }
 }
--- aluilcine/src/main/java/fr/iut/aluilcine/repositories/ReviewRepository.java
+++ aluilcine/src/main/java/fr/iut/aluilcine/repositories/ReviewRepository.java
@@ -1,11 +1,16 @@
 package fr.iut.aluilcine.repositories;
 
 import fr.iut.aluilcine.entities.Review;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 /**
  * Represente un repository des commentaires
  */
 public interface ReviewRepository extends MongoRepository<Review, String> {
+
+    Page<Review> findByMovieId(String movieId,Pageable pageable);
+
 
 }
